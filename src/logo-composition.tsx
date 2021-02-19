eca1b531544e652e8b272f9819a700f8bc701090
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2021-02-19 10:15:20 +0100

    Stating colors

--- src/logo-composition.tsx
+++ src/logo-composition.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Sequence} from 'remotion';
 import colors from './colors/colors.json';
 import {hexToRgb} from './helper/hexToRgb';
@@ -16,106 +16,108 @@ export type RGBColors = {
 export type HEXColors = typeof colors;
 
 export const LogoComposition = () => {
-	const {primary, secondary, red, dark} = Object.entries(colors).reduce(
+	const [hexColors, setHexColors] = useState(colors)
+	const [rgbColors, setRgbColors] = useState(Object.entries(hexColors).reduce(
 		(acc, [key, value]) => {
 			acc = {...acc, [`${key}`]: hexToRgb(value)};
 			return acc;
 		},
 		{} as RGBColors
-	);
+	));
+	const {primary, secondary, red, dark} = rgbColors;
 	const [r, g, b] = useKeyframes(
 		[
 			[20, 21],
 			[34, 35],
-			[76, 77],
+			[76, 77]
 		],
 		[
 			[
 				[primary.r, secondary.r],
 				[primary.g, secondary.g],
-				[primary.b, secondary.b],
+				[primary.b, secondary.b]
 			],
 			[
 				[secondary.r, red.r],
 				[secondary.g, red.g],
-				[secondary.b, red.b],
+				[secondary.b, red.b]
 			],
 			[
 				[red.r, dark.r],
 				[red.g, dark.g],
-				[red.b, dark.b],
-			],
+				[red.b, dark.b]
+			]
 		]
 	);
-
+	
 	return (
 		<div
 			style={{
 				flex: 1,
 				backgroundColor: `rgba(${r},${g},${b})`,
-				display: 'flex',
+				display: 'flex'
 			}}
 		>
 			<Sequence from={21} durationInFrames={6}>
 				<ExplosionLine
-					colors={colors}
+					colors={hexColors}
 					explosionWrapperStyle={{
 						left: '49%',
 						top: '48.6%',
-						transform: 'rotate(38deg)',
+						transform: 'rotate(38deg)'
 					}}
 					explosionLineStyle={{
 						width: '10px',
-						borderRadius: '5px',
+						borderRadius: '5px'
 					}}
 				/>
 				<ExplosionLine
-					colors={colors}
+					colors={hexColors}
 					explosionWrapperStyle={{
 						left: '49%',
 						top: '48.6%',
-						transform: 'rotate(218deg)',
+						transform: 'rotate(218deg)'
 					}}
 					explosionLineStyle={{
 						width: '10px',
-						borderRadius: '5px',
+						borderRadius: '5px'
 					}}
 				/>
 			</Sequence>
 			<Sequence from={22} durationInFrames={6}>
 				<ExplosionLine
-					colors={colors}
+					colors={hexColors}
 					explosionWrapperStyle={{
 						left: '49%',
 						top: '48.6%',
-						transform: 'rotate(90deg)',
+						transform: 'rotate(90deg)'
 					}}
 					explosionLineStyle={{
 						width: '10px',
-						borderRadius: '5px',
+						borderRadius: '5px'
 					}}
 				/>
 				<ExplosionLine
-					colors={colors}
+					colors={hexColors}
 					explosionWrapperStyle={{
 						left: '49%',
 						top: '48.6%',
-						transform: 'rotate(-90deg)',
+						transform: 'rotate(-90deg)'
 					}}
 					explosionLineStyle={{
 						width: '10px',
-						borderRadius: '5px',
+						borderRadius: '5px'
 					}}
 				/>
 			</Sequence>
 			<Sequence from={0} durationInFrames={35}>
-				<LogoLowerDash colors={colors} />
+				<LogoLowerDash colors={hexColors}/>
 			</Sequence>
 			<Sequence from={0} durationInFrames={35}>
-				<LogoUpperDash colors={colors} />
+				<LogoUpperDash colors={hexColors}/>
 			</Sequence>
 			<Sequence from={35} durationInFrames={75}>
-				<CirceAnimation colors={colors} />
+				<CirceAnimation colors={hexColors}/>
 			</Sequence>
 		</div>
 	);
